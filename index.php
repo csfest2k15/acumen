9046627cec742ca36598aa8f9275e755a458fe58
vasavi <csfest2k15@gmail.com>
2015-02-14 16:11:32 +0530

    intial commit 2

--- index.php
+++ index.php
@@ -12,7 +12,6 @@
     <link rel="shortcut icon" href="favicon.ico">
 
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/animate.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" type="text/css" href="css/default.css" />
@@ -20,6 +19,8 @@
     <link rel="stylesheet" type="text/css" href="css/component.css" />
     <link rel="stylesheet" type="text/css" href="css/animations.css" />
     <script src="js/modernizr.custom.js" type="text/javascript"></script>
+
+   
 
 </head>
 
@@ -74,85 +75,65 @@
 
             </div>
 
-            <div class="wrapper">
-                <div id="ei-slider" class="ei-slider">
-                    <ul class="ei-slider-large">
-                        <li>
-                            <img src="http://images.visitcanberra.com.au/images/canberra_hero_image.jpg" alt="image06" />
-                            <div class="ei-title">
-                                <h2>Accume CS FEST</h2>
-                                <h3>2015</h3>
-                            </div>
-                        </li>
-                        <li>
-                            <img src="http://images.visitcanberra.com.au/images/canberra_hero_image.jpg" alt="image01" />
-                            <div class="ei-title">
-                                <h2>Creative</h2>
-                                <h3>Duet</h3>
-                            </div>
-                        </li>
-                        <li>
-                            <img src="http://images.visitcanberra.com.au/images/canberra_hero_image.jpg" alt="image02" />
-                            <div class="ei-title">
-                                <h2>Friendly</h2>
-                                <h3>Devil</h3>
-                            </div>
-                        </li>
-                        <li>
-                            <img src="http://images.visitcanberra.com.au/images/canberra_hero_image.jpg" alt="image03" />
-                            <div class="ei-title">
-                                <h2>Something</h2>
-                                <h3>Something Else</h3>
-                            </div>
-                        </li>
-                        <li>
-                            <img src="http://images.visitcanberra.com.au/images/canberra_hero_image.jpg" alt="image04" />
-                            <div class="ei-title">
-                                <h2>Fun</h2>
-                                <h3>Masti</h3>
-                            </div>
-                        </li>
-                        <li>
-                            <img src="http://images.visitcanberra.com.au/images/canberra_hero_image.jpg" alt="image05" />
-                            <div class="ei-title">
-                                <h2>Loving</h2>
-                                <h3>Rebel</h3>
-                            </div>
-                        </li>
-                        <li>
-                            <img src="http://images.visitcanberra.com.au/images/canberra_hero_image.jpg" alt="image07" />
-                            <div class="ei-title">
-                                <h2>Crazy</h2>
-                                <h3>Friend</h3>
-                            </div>
-                        </li>
-                    </ul>
-                    <!-- ei-slider-large -->
-                    <ul class="ei-slider-thumbs">
-                        <li class="ei-slider-element">Current</li>
-                        <li><a href="#">Slide 6</a><img src="http://images.visitcanberra.com.au/images/canberra_hero_image.jpg" alt="thumb06" />
-                        </li>
-                        <li><a href="#">Slide 1</a><img src="http://images.visitcanberra.com.au/images/canberra_hero_image.jpg" alt="thumb01" />
-                        </li>
-                        <li><a href="#">Slide 2</a><img src="http://images.visitcanberra.com.au/images/canberra_hero_image.jpg" alt="thumb02" />
-                        </li>
-                        <li><a href="#">Slide 3</a><img src="http://images.visitcanberra.com.au/images/canberra_hero_image.jpg" alt="thumb03" />
-                        </li>
-                        <li><a href="#">Slide 4</a><img src="http://images.visitcanberra.com.au/images/canberra_hero_image.jpg" alt="thumb04" />
-                        </li>
-                        <li><a href="#">Slide 5</a><img src="http://images.visitcanberra.com.au/images/canberra_hero_image.jpg" alt="thumb05" />
-                        </li>
-                        <li><a href="#">Slide 7</a><img src="http://images.visitcanberra.com.au/images/canberra_hero_image.jpg" alt="thumb07" />
-                        </li>
-                    </ul>
-                    <!-- ei-slider-thumbs -->
+
+
+            <div id="slider1_container" class="" style="position: fixed; margin: 0 auto; bottom: 30px; width: 1300px; height: 450px; overflow: hidden;">
+                <!-- Loading Screen -->
+                <div u="loading" style="position: absolute; top: 0px; left: 0px;">
+                    <div style="filter: alpha(opacity=70); opacity: 0.7; position: absolute; display: block; top: 0px; left: 0px; width: 100%; height: 100%;">
+                    </div>
+                    <div style="position: absolute; display: block; background: url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuUOyPN4VE5s1gYaIZ5EAPImbQQEPkwuVDf8Zvevlqd8crdH22') no-repeat center center; top: 0px; left: 0px; width: 100%; height: 100%;">
+                    </div>
                 </div>
-                <!-- ei-slider -->
+                <!-- Slides Container -->
+                <div u="slides" style="cursor: move; position: absolute; left: 0px; top: 0px; width: 1300px; height: 500px; overflow: hidden;">
+                    <div>
+                        <img u="image" src="http://images.visitcanberra.com.au/images/canberra_hero_image.jpg" />
+                        <div style="position: absolute; width: 480px; height: 120px; top: 30px; left: 30px; padding: 5px; text-align: left; line-height: 60px; text-transform: uppercase; font-size: 50px;
+                        color: #000;">Accumen
+                        </div>
+                        <div style="position: absolute; width: 480px; height: 120px; top: 300px; left: 30px; padding: 5px; text-align: left; line-height: 36px; font-size: 30px; color: #FFFFFF;">
+                            CS Fest 2015
+                        </div>
+                    </div>
+                    <div>
+                        <img u="image" src="http://images.visitcanberra.com.au/images/canberra_hero_image.jpg" />
+                        <div style="position: absolute; width: 480px; height: 120px; top: 30px; left: 30px; padding: 5px; text-align: left; line-height: 60px; text-transform: uppercase; font-size: 50px; color: #000;">Creative
+                        </div>
+                        <div style="position: absolute; width: 480px; height: 120px; top: 300px; left: 30px; padding: 5px; text-align: left; line-height: 36px; font-size: 30px;
+                        color: #FFFFFF;">
+                            Awesomeness
+                        </div>
+                    </div>
+                    <div>
+                        <img u="image" src="http://images.visitcanberra.com.au/images/canberra_hero_image.jpg" />
+                        <div style="position: absolute; width: 480px; height: 120px; top: 30px; left: 30px; padding: 5px; text-align: left; line-height: 60px; text-transform: uppercase; font-size: 50px;
+                        color: #000;">Fun!!
+                        </div>
+                        <div style="position: absolute; width: 480px; height: 120px; top: 300px; left: 30px; padding: 5px; text-align: left; line-height: 36px; font-size: 30px;
+                        color: #FFFFFF;">
+                            Masti
+                        </div>
+                    </div>
+                </div>
 
+                <!-- Bullet Navigator Skin Begin -->
+
+                <!-- bullet navigator container -->
+                <div u="navigator" class="jssorb21" style="position: fixed; bottom: 36px; right: 50%">
+                    <!-- bullet navigator item prototype -->
+                    <div u="prototype" style="POSITION: absolute; WIDTH: 19px; HEIGHT: 19px; text-align:center; line-height:19px; color:White; font-size:12px;"></div>
+                </div>
+                <!-- Bullet Navigator Skin End -->
+
+                <!-- Arrow Navigator Skin Begin -->
+
+                <!-- Arrow Left -->
+                <span u="arrowleft" class="jssora21l" style="width: 55px; height: 55px; top: 123px; left: 8px;"></span>
+                <!-- Arrow Right -->
+                <span u="arrowright" class="jssora21r" style="width: 55px; height: 55px; top: 123px; right: 8px"></span>
+               
             </div>
-            <!-- wrapper -->
-
-
 
 
 
@@ -436,14 +417,202 @@
     </div>
 
 
-    <script type="text/javascript" src="js/jquery-2.1.3.min.js"></script>
-    <script type="text/javascript" src="js/jquery.eislideshow.js"></script>
-    <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
-    <script type="text/javascript" src="js/work.js"></script>
-
-
+  
 
 </body>
+
+
+
+ <script type="text/javascript" src="js/jquery-2.1.3.min.js"></script>
+    <script type="text/javascript" src="js/jssor.js"></script>
+    <script type="text/javascript" src="js/jssor.slider.js"></script>
+
+    <script>
+    jssor_slider1_starter = function(containerId) {
+        var _CaptionTransitions = [];
+        _CaptionTransitions["L"] = {
+            $Duration: 900,
+            x: 0.6,
+            $Easing: {
+                $Left: $JssorEasing$.$EaseInOutSine
+            },
+            $Opacity: 2
+        };
+        _CaptionTransitions["R"] = {
+            $Duration: 900,
+            x: -0.6,
+            $Easing: {
+                $Left: $JssorEasing$.$EaseInOutSine
+            },
+            $Opacity: 2
+        };
+        _CaptionTransitions["T"] = {
+            $Duration: 900,
+            y: 0.6,
+            $Easing: {
+                $Top: $JssorEasing$.$EaseInOutSine
+            },
+            $Opacity: 2
+        };
+        _CaptionTransitions["B"] = {
+            $Duration: 900,
+            y: -0.6,
+            $Easing: {
+                $Top: $JssorEasing$.$EaseInOutSine
+            },
+            $Opacity: 2
+        };
+        _CaptionTransitions["ZMF|10"] = {
+            $Duration: 900,
+            $Zoom: 11,
+            $Easing: {
+                $Zoom: $JssorEasing$.$EaseOutQuad,
+                $Opacity: $JssorEasing$.$EaseLinear
+            },
+            $Opacity: 2
+        };
+        _CaptionTransitions["RTT|10"] = {
+            $Duration: 900,
+            $Zoom: 11,
+            $Rotate: 1,
+            $Easing: {
+                $Zoom: $JssorEasing$.$EaseOutQuad,
+                $Opacity: $JssorEasing$.$EaseLinear,
+                $Rotate: $JssorEasing$.$EaseInExpo
+            },
+            $Opacity: 2,
+            $Round: {
+                $Rotate: 0.8
+            }
+        };
+        _CaptionTransitions["RTT|2"] = {
+            $Duration: 900,
+            $Zoom: 3,
+            $Rotate: 1,
+            $Easing: {
+                $Zoom: $JssorEasing$.$EaseInQuad,
+                $Opacity: $JssorEasing$.$EaseLinear,
+                $Rotate: $JssorEasing$.$EaseInQuad
+            },
+            $Opacity: 2,
+            $Round: {
+                $Rotate: 0.5
+            }
+        };
+        _CaptionTransitions["RTTL|BR"] = {
+            $Duration: 900,
+            x: -0.6,
+            y: -0.6,
+            $Zoom: 11,
+            $Rotate: 1,
+            $Easing: {
+                $Left: $JssorEasing$.$EaseInCubic,
+                $Top: $JssorEasing$.$EaseInCubic,
+                $Zoom: $JssorEasing$.$EaseInCubic,
+                $Opacity: $JssorEasing$.$EaseLinear,
+                $Rotate: $JssorEasing$.$EaseInCubic
+            },
+            $Opacity: 2,
+            $Round: {
+                $Rotate: 0.8
+            }
+        };
+        _CaptionTransitions["CLIP|LR"] = {
+            $Duration: 900,
+            $Clip: 15,
+            $Easing: {
+                $Clip: $JssorEasing$.$EaseInOutCubic
+            },
+            $Opacity: 2
+        };
+        _CaptionTransitions["MCLIP|L"] = {
+            $Duration: 900,
+            $Clip: 1,
+            $Move: true,
+            $Easing: {
+                $Clip: $JssorEasing$.$EaseInOutCubic
+            }
+        };
+        _CaptionTransitions["MCLIP|R"] = {
+            $Duration: 900,
+            $Clip: 2,
+            $Move: true,
+            $Easing: {
+                $Clip: $JssorEasing$.$EaseInOutCubic
+            }
+        };
+
+        var options = {
+            $FillMode: 2,
+            $AutoPlay: true,
+            $AutoPlayInterval: 4000,
+            $PauseOnHover: 1,
+            $ArrowKeyNavigation: true,
+            $SlideEasing: $JssorEasing$.$EaseOutQuint,
+            $SlideDuration: 800,
+            $MinDragOffsetToSlide: 20,
+            //$SlideWidth: 600,                                 
+            //$SlideHeight: 300,                               
+            $SlideSpacing: 0,
+            $DisplayPieces: 1,
+            $ParkingPosition: 0,
+            $UISearchMode: 1,
+            $PlayOrientation: 1,
+            $DragOrientation: 1,
+
+            $CaptionSliderOptions: {
+                $Class: $JssorCaptionSlider$,
+                $CaptionTransitions: _CaptionTransitions,
+                $PlayInMode: 1,
+                $PlayOutMode: 3
+            },
+
+            $BulletNavigatorOptions: {
+                $Class: $JssorBulletNavigator$,
+                $ChanceToShow: 2,
+                $Steps: 1,
+                $Lanes: 1,
+                $SpacingX: 8,
+                $SpacingY: 8,
+                $Orientation: 1,
+                $Scale: false
+            },
+
+            $ArrowNavigatorOptions: {
+                $Class: $JssorArrowNavigator$,
+                $ChanceToShow: 1,
+                $AutoCenter: 2,
+                $Steps: 1
+            }
+        };
+
+        var jssor_slider1 = new $JssorSlider$(containerId, options);
+
+        //responsive code begin
+        //you can remove responsive code if you don't want the slider scales while window resizes
+        function ScaleSlider() {
+            var bodyWidth = document.body.clientWidth;
+            if (bodyWidth)
+                jssor_slider1.$ScaleWidth(Math.min(bodyWidth, 1920));
+            else
+                $Jssor$.$Delay(ScaleSlider, 30);
+        }
+
+        ScaleSlider();
+        $Jssor$.$AddEvent(window, "load", ScaleSlider);
+
+        $Jssor$.$AddEvent(window, "resize", $Jssor$.$WindowResizeFilter(window, ScaleSlider));
+        $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
+        //responsive code end
+    };
+
+    jssor_slider1_starter('slider1_container');
+  
+    </script>
+
+  
+    <script type="text/javascript" src="js/work.js"></script>
+
 
 
 </html>
